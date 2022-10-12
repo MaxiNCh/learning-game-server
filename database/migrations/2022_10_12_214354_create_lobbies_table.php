b3f6fd23391e86db7f537666db6b7f72971e5d08
Maxim Zaboronkin <mvzaboronkin@gmail.com>
2022-10-13 00:49:27 +0300

    create lobbies table

---
+++ database/migrations/2022_10_12_214354_create_lobbies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lobbies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('game_id')->constrained();
+            $table->unsignedBigInteger('host_id');
+            $table->foreign('host_id')->references('id')->on('users');
+            $table->string('pincode', 20);
+            $table->string('status', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lobbies');
+    }
+};
