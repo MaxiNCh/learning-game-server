be24fa870837de50568fef0e527bf2e7b3271d41
Maxim Zaboronkin <mvzaboronkin@gmail.com>
2022-12-15 05:10:36 +0300

    change allow_origins in websockets

--- config/websockets.php
+++ config/websockets.php
@@ -46,7 +46,7 @@ return [
      * Leave this empty if you want to accept requests from all hosts.
      */
     'allowed_origins' => [
-        'localhost'
+        // 'localhost',
     ],
 
     /*
